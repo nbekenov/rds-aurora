807139f2edd5cf2faec75bd2530d3af295c5d0f2
nbekenov <nur.beken.usa@gmail.com>
2024-03-26 15:31:58 -0400

    add vpc endpoints

--- bootstrap/data.tf
+++ bootstrap/data.tf
@@ -2,3 +2,15 @@ data "aws_caller_identity" "current" {}
 data "aws_region" "current" {}
 data "aws_partition" "current" {}
 data "aws_availability_zones" "available" {}
+
+
+data "aws_subnets" "private" {
+  filter {
+    name   = "tag:Name"
+    values = ["${var.vpc_name}-private-*"]
+  }
+  filter {
+    name   = "vpc-id"
+    values = [module.vpc.vpc_id]
+  }
+}
